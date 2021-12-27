432457c89a776fe8c8ce8aba4e9844e429406f91
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2021-12-26 23:29:21 -0300

    Condições

---
+++ Modulo_D/Aula11/Exercicio08.js
@@ -0,0 +1,9 @@
+var vel = 12;
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+
+if (vel > 60) { // Condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!');
+}
+
+console.log('Dirija sempre utilizando o cinto de segurança!')
