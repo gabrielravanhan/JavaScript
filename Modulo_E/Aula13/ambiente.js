73dd675ab54ee2a40a09437488971b941654d14a
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2022-01-02 00:45:05 -0300

    Módulo E - Repetições

---
+++ Modulo_E/Aula13/ambiente.js
@@ -0,0 +1,24 @@
+var cont = 1;
+
+do {
+    console.log(`${cont}ª vez - JavaScript é maravilhoso 🥵`);
+    cont++;
+} while (cont <= 6)
+
+/*
+var cont = 1;
+
+while (cont <= 6) {
+    console.log(`${cont}ª vez - JavaScript é maravilhoso 🥵`);
+    cont++;
+}
+*/
+
+/*
+console.log('1ª vez - JavaScript é maravilhoso 🥵');
+console.log('2ª vez - JavaScript é maravilhoso 🥵');
+console.log('3ª vez - JavaScript é maravilhoso 🥵');
+console.log('4ª vez - JavaScript é maravilhoso 🥵');
+console.log('5ª vez - JavaScript é maravilhoso 🥵');
+console.log('6ª vez - JavaScript é maravilhoso 🥵');
+*/
