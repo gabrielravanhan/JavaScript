ea1ab16a05825e170cdbab3926f40cc1733f7208
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2022-01-03 16:00:55 -0300

    Aprimoramento da resolução do exercício com base em sua correção

--- Modulo_E/Aula14Exercicios/Exercicio16/script.js
+++ Modulo_E/Aula14Exercicios/Exercicio16/script.js
@@ -1,46 +1,32 @@
 function contar() {
-    var inicio = document.getElementById('inicio');
-    var fim = document.getElementById('fim');
-    var passo = document.getElementById('passo');
-    var contagem = document.getElementById('contagem');
-
-    var valorInicio = Number(inicio.value);
-    var valorFim = Number(fim.value);
-    var valorPasso = Number(passo.value);
+    let inicio = document.getElementById('inicio');
+    let fim = document.getElementById('fim');
+    let passo = document.getElementById('passo');
+    let contagem = document.getElementById('contagem');
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        contagem.innerHTML = `<p>Impossível contar!</p>`;
+        contagem.innerHTML = 'Impossível contar!';
     } else {
-        contagem.innerHTML = `Contando:<br/>`;
+        contagem.innerHTML = 'Contando:<br/>';
 
-        if (valorPasso == 0) {
+        let valorInicio = Number(inicio.value);
+        let valorFim = Number(fim.value);
+        let valorPasso = Number(passo.value);
+
+        if (valorPasso <= 0) {
             alert('Passo inválido! Considerando PASSO 1');
+            valorPasso = 1;
+        }
 
-            if (valorInicio > valorFim) {
-                for (var i = valorInicio; i >= valorFim; i--) {
-                    var span = document.createElement('span');
-                    span.innerHTML = `${i} `;
-                    contagem.appendChild(span);
-                }
-            } else {
-                for (var i = valorInicio; i <= valorFim; i++) {
-                    var span = document.createElement('span');
-                    span.innerHTML = `${i} `;
-                    contagem.appendChild(span);
-                }
-            }
-        } else if (valorInicio > valorFim) {
-            for (var i = valorInicio; i >= valorFim; i = i - valorPasso) {
-                var span = document.createElement('span');
-                span.innerHTML = `${i} `;
-                contagem.appendChild(span);
+        if (valorInicio > valorFim) {
+            for (let i = valorInicio; i >= valorFim; i -= valorPasso) {
+                contagem.innerHTML += ` ${i} \u{1F449}`; // \u{1F449} = código do emoji da mão apontando o dedo para o lado
             }
         } else {
-            for (var i = valorInicio; i <= valorFim; i = i + valorPasso) {
-                var span = document.createElement('span');
-                span.innerHTML = `${i} `;
-                contagem.appendChild(span);
+            for (let i = valorInicio; i <= valorFim; i += valorPasso) {
+                contagem.innerHTML += `${i} \u{1F449}`; // \u{1F449} = código do emoji da mão apontando o dedo para o lado
             }
         }
+        contagem.innerHTML += `\u{1F3C1}`; // \u{1F3C1} = código do emoji da bandeira
     }
 }
