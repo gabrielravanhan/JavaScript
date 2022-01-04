fd05f8780bcf1716d29c5854f3310937da5e5d2b
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2022-01-04 15:16:40 -0300

    outro exercício

--- Modulo_E/Aula14Exercicios/Exercicio17/script.js
+++ Modulo_E/Aula14Exercicios/Exercicio17/script.js
@@ -1,1 +1,20 @@
-alert('Olá!');
+function gerar() {
+    let numero = document.getElementById('numero');
+    let tabuada = document.getElementById('tabuada');
+
+    if (numero.value.length == 0) {
+        alert('Digite um número!')
+    } else {
+        let valorNumero = Number(numero.value);
+        tabuada.innerHTML = '';
+
+        for (let i = 0; i <= 10; i++) {
+            let opcao = document.createElement('option');
+            let mult = valorNumero * i;
+
+            opcao.text = `${valorNumero} X ${i} = ${mult}`;
+            opcao.value = `tab${i}`
+            tabuada.appendChild(opcao);
+        }
+    }
+}
