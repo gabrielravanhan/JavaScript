1e46c0e574641e1cd15323d17776bf974fe24c5c
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2022-01-11 21:25:03 -0300

    Variáveis e constantes

---
+++ JavaScript para iniciantes/01 - Variáveis/script.js
@@ -0,0 +1,12 @@
+// armazenam dados
+// declaradas através das palavras reservadas "var" ou "let"
+// não podem começar com números
+// sem espaço entra as palavras
+// camelCase = primeira letra da primeira palavra é minúscula, já as primeiras letras das palavras seguintes são maiúsculas
+let nomeCompleto = 'Sidney Luna de Andrade';
+let dataNascimento = '12/12/1969';
+let email = 'andrade.sl@outlook.com';
+
+console.log(`Nome completo: ${nomeCompleto}`);
+console.log(`Data de nascimento: ${dataNascimento}`);
+console.log(`E-mail: ${email}`);
---
+++ JavaScript para iniciantes/02 - Constantes/script.js
@@ -0,0 +1,3 @@
+// são variáveis que não podem ter seus valores iniciais alterados
+const valorIngressoAdulto = 30;
+console.log(`O valor do ingresso para adultos é: ${valorIngressoAdulto}R$`);
