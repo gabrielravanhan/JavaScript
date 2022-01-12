7d2cd25313ebd4285ee480bee6ca19fc662f63cb
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2022-01-11 22:06:29 -0300

    Arrays

---
+++ JavaScript para iniciantes/06 - Arrays/script.js
@@ -0,0 +1,10 @@
+// Arrays = conjunto de dados, acessado por um ídice iniciado por 0
+// Array é um tipo de objeto
+
+let familia = [15, true, 'Rogério', 19];
+
+console.log(familia[2]);
+console.log(familia.length);
+
+let amigo = ['Jônatas', 17, 'Lençóis Paulista', 'São Paulo'];
+console.log(amigo);
