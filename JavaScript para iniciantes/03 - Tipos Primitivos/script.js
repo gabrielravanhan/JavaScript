85baf387a007e9f1f374f63f412c8e8da1c7aec0
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2022-01-11 21:45:12 -0300

    Tipagem Dinâmica

--- JavaScript para iniciantes/03 - Tipos Primitivos/script.js
+++ JavaScript para iniciantes/03 - Tipos Primitivos/script.js
@@ -2,4 +2,4 @@ let nome = 'Gabriel'; // string literal
 let idade = 17; // number literal
 let estaAprovado = true; // boolean
 let sobrenome; // undefined
-let corSelecionada = null // null - usado para redefinir um valor
+let corSelecionada = null; // null - usado para redefinir um valor
---
+++ JavaScript para iniciantes/04 - Tipagem Dinâmica/script.js
@@ -0,0 +1,7 @@
+// não é necessário declarar o tipo de uma variável, o JavaScript o identifica automaticamente
+// os tipos podem mudar com base no valor informado
+let nome = 'Beto';
+console.log(typeof nome);
+
+nome = 25;
+console.log(typeof nome);
