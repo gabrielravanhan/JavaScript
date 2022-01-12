2cc610b51f355e5f3c409b54ac46b34d709e0c2c
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2022-01-11 21:39:16 -0300

    Tipos Primitivos

---
+++ JavaScript para iniciantes/03 - Tipos Primitivos/script.js
@@ -0,0 +1,5 @@
+let nome = 'Gabriel'; // string literal
+let idade = 17; // number literal
+let estaAprovado = true; // boolean
+let sobrenome; // undefined
+let corSelecionada = null // null - usado para redefinir um valor
