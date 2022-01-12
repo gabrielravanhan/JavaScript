658363bb83f2b2067aa34e2ce11baff852926196
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2022-01-11 21:54:25 -0300

    Objetos

---
+++ JavaScript para iniciantes/05 - Objetos/script.js
@@ -0,0 +1,11 @@
+let pessoa = {
+    nome: 'Gabriel',
+    idade: 17,
+    estaAprovado: true,
+    sobrenome: 'Ravanhan',
+    telefone: '12356789',
+    nacionalidade: 'Brasileiro',
+    sexo: 'M'
+};
+
+console.log(pessoa);
