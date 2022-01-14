920de4abbe501e8461e674d6cb16c369c030f868
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2022-01-14 20:47:37 -0300

    Tipos de Funções

---
+++ JavaScript para iniciantes/08 - Tipos de Funções/script.js
@@ -0,0 +1,15 @@
+// Realiza uma tarefa e não retorna nada
+function dizerNome() {
+    console.log('Gabriel');
+}
+
+dizerNome();
+
+// Realiza uma tarefa e retorna algo
+function multiplicarPorDois(valor) {
+    return valor * 2;
+}
+
+let resultado = multiplicarPorDois(10);
+
+console.log(resultado);
