b8e3cf2d346e77a492e5242e420ef00bd9b8afad
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2022-01-26 21:45:58 -0300

    Math

---
+++ JavaScript para iniciantes/31 - Math/script.js
@@ -0,0 +1,16 @@
+// Função Math
+
+// Chama um número aleatório entre 0 e 1
+console.log(Math.random());
+
+// Mostra o maior valor
+console.log(Math.max(1, 2, 15, 10));
+
+// Mostra o menor valor
+console.log(Math.min(1, 2, 15, 10));
+
+// Calcula potências
+console.log(Math.pow(10, 3));
+
+// Calcula raízes quadrada
+console.log(Math.sqrt(9));
