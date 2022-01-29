a37b1bb5192fb5385496c6f98744419e83f38c62
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2022-01-28 21:45:02 -0300

    Mini-Projeto 13

---
+++ JavaScript para iniciantes/37 - (Mini-Projeto 13) - Montador de Postagem de Blog/script.js
@@ -0,0 +1,11 @@
+let postagem = {
+    titulo: 'As Magníficas Aventuras de Ro Furlan',
+    mensagem: '😎😎😎',
+    visualizacoes: 255,
+    comentarios: [
+        {autor: 'Luis Fernando', mensagem: 'Muito bom'}
+    ],
+    estaAoVivo: true
+}
+
+console.log(postagem);
